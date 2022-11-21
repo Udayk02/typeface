a63a88ae4f637d9f29ecf38306e03276b2c94b77
Uday Kiran <udayjyo02@gmail.com>
2022-11-21 16:05:13 +0530

    Solved 3rd problem

--- 3.cpp
+++ 3.cpp
@@ -3,3 +3,27 @@
 
 using namespace std;
 
+int valid(string n) {
+    for(int i = 0; i < n.length(); ++i) {
+        if(n[i] == '3' || n[i] == '4' || n[i] == '7') {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int count;
+    cin >> count;
+    int i = 0;
+
+    for(int num = 1;; num++) {
+        if(valid(to_string(num))) {
+            i++;
+            if(i == count) {
+                cout << num;
+                return 0;
+            }
+        }
+    }
+}
